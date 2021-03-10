c63e7ad3414ef1dc59cdd96f24290e7b7b8736bd
Ekaterina Volkova <volkovakaterin@gmail.com>
2021-03-11 00:35:57 +0400

    files changed

--- src/js/class.js
+++ src/js/class.js
@@ -17,22 +17,6 @@ class Character {
     this.attack = attack;
     this.defence = defence;
   }
-
-  // eslint-disable-next-line consistent-return
-  levelUp() {
-    if (this.health !== 0) {
-      this.level += 1;
-      this.attack += (this.attack / 100 * 25);
-      this.defence += (this.defence / 100 * 25);
-      this.health = 100;
-    } else { return new Error('Нельзя повысить уровень погибшего'); }
-  }
-
-  damage(points) {
-    if (this.health >= 0) {
-      this.health -= points * (1 - this.defence / 100);
-    }
-  }
 }
 
 class Bowman extends Character {
